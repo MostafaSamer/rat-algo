87d700aed2b90fb932159c1ae3a1c5ed03307ebe
root <root@mostafa-pc>
2024-02-04 20:39:59 +0200

    .

--- Chapter3/recursive.js
+++ Chapter3/recursive.js
@@ -9,5 +9,22 @@ function fact(x) {
     else return x * fact(x-1)
 }
 
+function sum(arr) {
+    let total = 0;
+    arr.forEach(x => {
+        total += x;
+    });
+    return total;
+}
+
+function sum_rec(arr) {
+    if (arr.length === 0) return 0;
+    let x = arr.shift();
+    return x + sum_rec(arr);
+}
+
+
 // countdown(7)
 console.log(fact(5))
+console.log(sum([1, 2, 3, 4, 5, 6, 7]))
+console.log(sum_rec([1, 2, 3, 4, 5, 6, 7]))
